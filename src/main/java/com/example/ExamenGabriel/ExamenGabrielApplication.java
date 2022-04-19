f6a4f587462e3772a61af61e650055275c7d9945
Pardo2273 <mpita38@gmail.com>
2022-04-18 19:48:30 -0600

    Prueba a ver si logro ganarme 3 puntos extras

---
+++ src/main/java/com/example/ExamenGabriel/ExamenGabrielApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenGabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenGabrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenGabriel/ExamenGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenGabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
